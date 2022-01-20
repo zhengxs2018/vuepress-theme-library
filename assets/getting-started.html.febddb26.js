49d7a4f94ef189ea57f85048ae4202e347da2bb8
zhengxs <zhenxs2018@foxmail.com>
2022-01-20 21:39:04 +0800

    Updates

--- assets/getting-started.html.febddb26.js
+++
@@ -1,9 +0,0 @@
-import{d as e}from"./app.72821b37.js";import{_ as s}from"./plugin-vue_export-helper.21dcd24c.js";const n={},a=e(`<h1 id="\u5FEB\u901F\u4E0A\u624B" tabindex="-1"><a class="header-anchor" href="#\u5FEB\u901F\u4E0A\u624B" aria-hidden="true">#</a> \u5FEB\u901F\u4E0A\u624B</h1><blockquote><p>\u6B63\u5728\u79EF\u6781\u5F00\u53D1\u4E2D</p></blockquote><a class="npm-badge" href="http://npmjs.com/package/@zhengxs/vuepress-theme-library" title="@zhengxs/vuepress-theme-library" target="_blank" rel="noopener noreferrer"><img src="https://badgen.net/npm/v/@zhengxs/vuepress-theme-library/latest?label=@zhengxs/vuepress-theme-library" alt="@zhengxs/vuepress-theme-library@latest"></a><h2 id="\u5B89\u88C5" tabindex="-1"><a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a> \u5B89\u88C5</h2><p>\u5728\u547D\u4EE4\u884C\u4E2D\u8F93\u5165\u6267\u884C\u4EE5\u4E0B\u547D\u4EE4:</p><div class="language-bash ext-sh"><pre class="language-bash"><code><span class="token comment"># With NPM</span>
-$ <span class="token function">npm</span> <span class="token function">install</span> @zhengxs/vuepress-theme-library --save
-
-<span class="token comment"># With Yarn</span>
-$ <span class="token function">yarn</span> <span class="token function">add</span> @zhengxs/vuepress-theme-library
-
-<span class="token comment"># With PNPM</span>
-$ <span class="token function">pnpm</span> <span class="token function">add</span> @zhengxs/vuepress-theme-library
-</code></pre></div>`,6);function t(r,p){return a}var h=s(n,[["render",t]]);export{h as default};
